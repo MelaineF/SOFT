7ecd20a48e65b5805a1d6b991759673c6e2979bb
Valentin <asval.contactme@gmail.com>
2022-11-23 16:45:54 +0100

    improved register UX

--- lib/features/signup_page/presentation/page/register_page.dart
+++ lib/features/signup_page/presentation/page/register_page.dart
@@ -1,3 +1,4 @@
+import 'package:validators/validators.dart';
 import 'package:Swipe/core/firebase/database.dart';
 import 'package:Swipe/core/helper/logger.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,7 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController email = TextEditingController();
   TextEditingController firstname = TextEditingController();
   TextEditingController lastname = TextEditingController();
@@ -29,69 +31,88 @@ class _RegisterPageState extends State<RegisterPage> {
           },
           child: FocusTraversalGroup(
             child: Form(
-              autovalidateMode: AutovalidateMode.always,
-              onChanged: () {
-                Form.of(primaryFocus!.context!)!.save();
-              },
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.disabled,
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
                   child: Wrap(
-                runSpacing: 8.0,
-                children: <Widget>[
-                  TextFormField(
-                    controller: email,
-                    decoration: InputDecoration(
-                      border: const OutlineInputBorder(),
-                      filled: true,
-                      fillColor: Colors.blue.shade100,
-                      labelText: "Email"
-                    ),
-                    validator: (value) {
-                      // TODO: do proper check here
-                      if (value?.isEmpty ?? true) {
-                        return 'Please enter a valid email.';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextField(
-                    controller: firstname,
-                    decoration: InputDecoration(
-                      border: const OutlineInputBorder(),
-                      filled: true,
-                      fillColor: Colors.blue.shade100,
-                      labelText: "First Name"
-                    ),
-                  ),
-                  TextField(
-                    controller: lastname,
-                    decoration: InputDecoration(
-                      border: const OutlineInputBorder(),
-                      filled: true,
-                      fillColor: Colors.blue.shade100,
-                      labelText: "Last Name"
-                    ),
-                  ),
-                  TextField(
-                    controller: password,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                    ),
-                    obscureText: true,
-                  ),
-                  TextButton(onPressed: () {
-                    logger.d("login pressed");
-                    var rtdb = RealtimeDb();
-                    var added = rtdb.addUser(firstname.text, lastname.text, email.text, password.text);
-                    logger.d('is user added? $added');
+                    runSpacing: 8.0,
+                    alignment: WrapAlignment.center,
+                    children: <Widget>[
+                      TextFormField(
+                        controller: email,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Email"
+                        ),
+                        validator: (value) {
+                          if (!isEmail(value ?? "")) {
+                            return 'Please enter a valid email.';
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        controller: firstname,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "First Name"
+                        ),
+                        validator: (value) {
+                          if (isNull(value ?? "")) {
+                            return 'Your first name can\'t be blank.';
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        controller: lastname,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Last Name"
+                        ),
+                        validator: (value) {
+                          if (isNull(value ?? "")) {
+                            return 'Your last name can\'t be blank.';
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        controller: password,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Password',
+                        ),
+                        obscureText: true,
+                        validator: (value) {
+                          if (isNull(value ?? "")) {
+                            return 'Your password can\'t be blank.';
+                          }
+                          return null;
+                        },
+                      ),
+                      ElevatedButton(onPressed: () {
+                        logger.d("login pressed");
+                        if (_formKey.currentState?.validate() ?? false) {
+                          var rtdb = RealtimeDb();
+                          var added = rtdb.addUser(firstname.text, lastname.text, email.text, password.text);
+                          logger.d('is user added? $added');
 
-                    firstname.clear();
-                    lastname.clear();
-                    email.clear();
-                    password.clear();
-                  }, child: const Text("Login"))
-                ],
+                          _formKey.currentState?.reset();
+                          firstname.clear();
+                          lastname.clear();
+                          email.clear();
+                          password.clear();
+                        } else {
+                          logger.e("Could not add user, something is missing");
+                        }
+                      }, child: const Text("Register"))
+                    ]
               )),
             ),
           ),
